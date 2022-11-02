eda1ef3d10803cbd3d59f18ed7b6c2383bc41e50
Olive Riggs <rariggs@cpp.edu>
2022-11-02 15:05:36 -0700

    add display employee portal

--- Create_Employee.py
+++ Create_Employee.py
@@ -1,10 +1,12 @@
 import tkinter as tk  # GUI module
 from PIL import ImageTk  # Display background
-import tkinter.font as font
+from tkinter import *
 
 
 def main():
 
+
+# Employee class
 #------ get functions will call get user's input, creates a new object------------------------------------------------
     def getFirstName(first):
         # user input  is saved into variables
@@ -27,7 +29,36 @@ def main():
 
     def getSalary(salary):
         salary = salary_entry.get()
-# --------------------------------------------------------------------------------------------------------------------
+
+    def printDataInput(first, last, email, dob, phone, position, earn):
+        root = tk.Tk()
+        root.title("Display Employee")
+        root.geometry('1200x800')
+
+        print_fullname = "Fullname     : " + str(first.get()) + " " + str(last.get())
+        full_name = Label(root, text=print_fullname, font=("Helvetica", 21), justify=LEFT)
+        full_name.pack()
+
+        print_email = "Email        : " + str(email.get())
+        display_email = Label(root, text=print_email, font=("Helvetica", 21), justify=LEFT)
+        display_email.pack()
+
+        print_dob = "Date of Birth: " + str(dob.get())
+        display_dob = Label(root, text=print_dob, font=("Helvetica", 21), justify=LEFT)
+        display_dob.pack()
+
+        print_phone = "Phone        : " + str(phone.get())
+        display_phone = Label(root, text=print_phone, font=("Helvetica", 21), justify=LEFT)
+        display_phone.pack()
+
+        print_position = "Position     : " + str(position.get())
+        display_position = Label(root, text=print_position, font=("Helvetica", 21), justify=LEFT)
+        display_position.pack()
+
+        print_earning = "Pay         : $ " + str(earn.get()) + "per hour"
+        display_earn = Label(root, text=print_earning, font=("Helvetica", 21), justify=LEFT)
+        display_earn.pack()
+        root.mainloop()
 
 
 #-----following code pertains to main user input window---------------------------------------------------------------
@@ -60,26 +91,35 @@ def main():
 
     # create Entry text boxes
     first_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    first_entry.pack()
     last_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    last_entry.pack()
     email_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    email_entry.pack()
     dob_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    dob_entry.pack()
     phone_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    phone_entry.pack()
     position_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
+    position_entry.pack()
     salary_entry = tk.Entry(my_canvas, font=("Helvetica", 12), width=50, bg="white", borderwidth=2)
-
-    first_entry_window = my_canvas.create_window(375, 125, anchor="nw", window=first_entry, command = getFirstName(first_entry))
-    last_entry_window = my_canvas.create_window(375, 175, anchor="nw", window=last_entry, command = getLastname(last_entry))
-    email_entry_window = my_canvas.create_window(375, 225, anchor="nw", window=email_entry, command = getEmail(email_entry))
-    dob_entry_window = my_canvas.create_window(375, 275, anchor="nw", window=dob_entry, command = getDOB(dob_entry))
-    phone_entry_window = my_canvas.create_window(375, 325, anchor="nw", window=phone_entry, command = getPhone(phone_entry))
-    position_entry_window = my_canvas.create_window(375, 375, anchor="nw", window=position_entry, command = getPosition(position_entry))
-    salary_entry_window = my_canvas.create_window(375, 425, anchor="nw", window=salary_entry, command = getSalary(salary_entry))
+    salary_entry.pack()
 
 
-    # We used the Button function in Tkinter which will call the function once clicked
+    first_entry_window = my_canvas.create_window(375, 125, anchor="nw", window=first_entry)
+    last_entry_window = my_canvas.create_window(375, 175, anchor="nw", window=last_entry)
+    email_entry_window = my_canvas.create_window(375, 225, anchor="nw", window=email_entry)
+    dob_entry_window = my_canvas.create_window(375, 275, anchor="nw", window=dob_entry)
+    phone_entry_window = my_canvas.create_window(375, 325, anchor="nw", window=phone_entry)
+    position_entry_window = my_canvas.create_window(375, 375, anchor="nw", window=position_entry)
+    salary_entry_window = my_canvas.create_window(375, 425, anchor="nw", window=salary_entry)
+
+
+    # We used the Button function in Tkinter which will call the function once user click save
     # Once user hit save, the system validates input and check whether account exists or not.
     create_button = tk.Button(root, text="Save", activeforeground='white', font=("Helvetica", 15), \
-                              width=15, height=20, borderwidth=2, command=lambda: getSalary(salary_entry))
+                              width=15, height=20, borderwidth=2,
+                              command=lambda: printDataInput(first_entry, last_entry, email_entry, dob_entry, phone_entry, position_entry, salary_entry))
 
     create_button_window = my_canvas.create_window(550, 550, height=35, anchor="nw", window=create_button)
 
