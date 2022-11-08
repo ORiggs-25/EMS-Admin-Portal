fee7330afc14d5a4a429ecce71400ffa6e3b86c3
Joshua Furman <jafbird52@gmail.com>
2022-11-07 20:15:36 -0800

    Finsished ViewAllEmployees, still need to make it look pretty

--- ViewAllEmployees.py
+++ ViewAllEmployees.py
@@ -1,3 +1,7 @@
+import tkinter
+from tkinter.constants import LEFT
+from tkinter.ttk import Label
+
 import requests
 import json
 
@@ -7,21 +11,81 @@ import tkinter.font as font
 
 
 def viewAllEmployees():
-    # Will Display all the employees in the database in terminal
-
+    global lastN, birthday, status, address, id, email, phone, firstN, roleId
     headers = {
         "Content-Type": "application/json",
         "Connection": "keep-alive",
     }
 
     try:
+
+        root = tk.Tk()
+        # renames the title of the window
+        root.title("Admin Portal")
+        # sets the dimensions of the window to measurement
+        root.geometry('1200x800')
+        # prevents user to resize the window
+        root.resizable(width=False, height=False)
+
+        # canvas function will create a background for the GUI
+        my_canvas = tk.Canvas(root, width=400, height=200, bd=0, highlightthickness=0)
+        my_canvas.pack(fill="both", expand=True)
+
         request = requests.get("https://uhwxroslh0.execute-api.us-east-1.amazonaws.com/dev/employees")
         employeesFromResponse = request.json()["Items"]
-        employees = []
+        x = -100
+        y = 200
         for emp in employeesFromResponse:
-            print(emp)
+            if x > 1000:
+                x = -100
+                y += 200
+            lastN = emp["lastName"]
+            birthday = emp["dob"]
+            status = emp["status"]
+            address = emp["address"]
+            id = emp["id"]
+            email = emp["email"]
+            phone = emp["phone"]
+            firstN = emp["firstName"]
+            roleId = emp["roleID"]
+            x += 250
+
+            for theEmp in range(9):
+
+                if theEmp == 1:
+                    print("debug")
+                    my_canvas.create_text(x, y, text="Last Name: " + lastN, font=("Helvetica", 10), fill="black")
+                elif theEmp == 2:
+                    my_canvas.create_text(x, y-20, text="Birthday: " + birthday, font=("Helvetica", 10), fill="black")
+                elif theEmp == 3:
+                    my_canvas.create_text(x, y-40, text="Status : " + status, font=("Helvetica", 10), fill="black")
+                elif theEmp == 4:
+                    my_canvas.create_text(x, y-60, text="Address: " + address, font=("Helvetica", 10), fill="black")
+                elif theEmp == 5:
+                    my_canvas.create_text(x, y-80, text="ID: " + str(id), font=("Helvetica", 10), fill="black")
+                elif theEmp == 6:
+                    my_canvas.create_text(x, y-100, text="Email: " + email, font=("Helvetica", 10), fill="black")
+                elif theEmp == 7:
+                    my_canvas.create_text(x, y-120, text="Phone: " + phone, font=("Helvetica", 10), fill="black")
+                elif theEmp == 8:
+                    my_canvas.create_text(x, y-140, text="First Name: " + firstN, font=("Helvetica", 10), fill="black")
+                elif theEmp == 9:
+                    my_canvas.create_text(x, y-160, text="Role ID: " + str(roleId), font=("Helvetica", 10), fill="black")
     except requests.exceptions.HTTPError as err:
         print(err)
+
+
+"""
+            my_canvas.create_text(600, 500, text="Last Name: " + lastN, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 450, text="Birthday: " + birthday, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 400, text="Status : " + status, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 350, text="Address: " + address, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 300, text="ID: " + str(id), font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 250, text="Email: " + email, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 200, text="Phone: " + phone, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 150, text="First Name: " + firstN, font=("Helvetica", 10), fill="black")
+            my_canvas.create_text(600, 100, text="Role ID: " + str(roleId), font=("Helvetica", 20), fill="black")
+"""
 
 
 def main():
